d053f8d5fe094bcf3fe72a0bc2ce25a823046610
Micke Larsson <mikael.larsson@laget.se>
2017-02-08 16:29:48 +0100

    Initial commit

---
+++ src/AzureEnquer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace laget.azure_enquer
+{
+    public interface IAzureEnquer
+    {
+        void Enqueue(string queueName, dynamic msg);
+        void Enqueue(string queueName, string msg);
+    }
+
+    public class AzureEnquer : IAzureEnquer
+    {
+        readonly CloudQueueClient _client;
+
+        public AzureEnquer(string cloudStorageAccountConnStr)
+        {
+            var storageAccount = CloudStorageAccount.Parse(cloudStorageAccountConnStr);
+            _client = storageAccount.CreateCloudQueueClient();
+        }
+
+        public void Enqueue(string queueName, dynamic msg)
+        {
+            Enqueue(queueName, JsonConvert.SerializeObject(msg));
+        }
+
+        public void Enqueue(string queueName, string msg)
+        {
+            var queue = _client.GetQueueReference(queueName);
+            queue.CreateIfNotExists();
+
+            var message = new CloudQueueMessage(JsonConvert.SerializeObject(msg));
+            queue.AddMessage(message);
+        }
+    }
+}
